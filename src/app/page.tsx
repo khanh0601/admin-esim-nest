f387035da828fd39ebc7bb085acab907fc19da68
Khanh0601 <tranquangkhanh2k1qv@gmail.com>
2025-06-08 23:00:57 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-export default function Home() {
-  const cookieStore = cookies();
-  const token =  cookieStore.get("token")?.value;
+export default async function Home() {
+  const cookieStore = await cookies();
+  console.log(cookieStore);
+  const token = cookieStore.get("token")?.value;
 
   if (!token) {
     redirect("/login");
